fcdb7ffef2458ab33e1a6e147295cb55c43405d4
sean-gibbonslawrie-roa-23 <78052448+SeanGibbons-Lawrie@users.noreply.github.com>
2023-11-10 08:26:17 +1300

    Update Overworld.js
    removed the basic player stat test in here

--- Overworld.js
+++ Overworld.js
@@ -3,26 +3,23 @@ class Overworld {
     this.element = config.element
     this.canvas = this.element.querySelector('.game-canvas')
     this.ctx = this.canvas.getContext('2d')
-    this.map = null //Maybe don't need this
+    // this.map = null //Maybe don't need this
   }
 
   init() {
+    
     this.startGameLoop()
+    this.hud = new Hud(this.element, 'player_stats')
+    this.hud.init(this.element)
 
     const image = new Image()
     image.onload = () => {
       this.ctx.drawImage(image, 0, 0)
     }
-    image.src = 'images/Background1.png'
+    image.src = 'images/seamlessdarkbg.png'
 
-    const div = document.createElement('div')
-    div.classList.add('player_stats')
-    const playerStatsText = document.createElement('h1')
-    playerStatsText.textContent = "Player Stats";
 
-    // Append the div to the canvas container
-    this.element.appendChild(div)
-    div.appendChild(playerStatsText)
+
   }
 
   startGameLoop() {
@@ -35,15 +32,3 @@ class Overworld {
   }
 }
 
-// const div = document.createElement("div");
-// div.style.width = "100px"; // Set the width of the div
-// div.style.height = "100px"; // Set the height of the div
-// div.style.backgroundColor = "blue"; // Set the background color
-// div.style.position = "absolute"; // Set the position to absolute
-
-// // Position the div on the canvas (adjust the coordinates as needed)
-// div.style.left = "50px";
-// div.style.top = "50px";
-
-// Append the div to the canvas container
-// this.element.appendChild(div);
