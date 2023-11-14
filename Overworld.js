f46776a03a1eb8a4d369838f617eea46db0bc4f4
sean-gibbonslawrie-roa-23 <78052448+SeanGibbons-Lawrie@users.noreply.github.com>
2023-11-14 16:38:36 +1300

    Update Overworld.js
    Added drawing images and objects to gameloop commented out hardcoded divs

--- Overworld.js
+++ Overworld.js
@@ -6,31 +6,78 @@ class Overworld {
     this.map = null //Maybe don't need this
   }
 
-  init() {
-    let str = 1;
-    this.startGameLoop()
-    this.player_stats_title = new Hud(this.element, 'player_stats_title')
-    console.log(this.player_stats_title)
-    this.player_stats_title.init(this.element)
-
-    this.player_stats = new Hud(this.element, 'player_stats')
-    console.log(this.player_stats)
-    this.player_stats.init(this.element)
-    this.player_stats.element.textContent = `Str: ${str}`
-
-    const image = new Image()
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0)
-    }
-    image.src = 'images/Background1.png'
-  }
-
   startGameLoop() {
     const step = () => {
+
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+      this.map.drawLowerImage(this.ctx)
+      
+      Object.values(this.map.gameObjects).forEach(object => {
+        
+
+        object.sprite.draw(this.ctx)
+      })
+
+      this.map.drawUpperImage(this.ctx)
+
+      console.log("stepping")
       requestAnimationFrame(() => {
         step()
       })
     }
     step()
+  }
+
+  init() {
+    
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom)
+    this.startGameLoop()
+
+    // let str = 1;
+    // let time = 3;
+    // this.player_stats_title = new Hud(this.element, 'player_stats_title')
+    // console.log(this.player_stats_title)
+    // this.player_stats_title.init(this.element)
+
+    // this.player_stats = new Hud(this.element, 'player_stats')
+    // console.log(this.player_stats)
+    // this.player_stats.init(this.element)
+    // this.player_stats.element.innerHTML = `<p>Str: ${str}<br>
+    // Time: ${time}</p>`
+
+    // this.choices = new Hud(this.element, 'choices_container')
+    // console.log(this.choices)
+    // this.choices.init(this.element)
+
+    // this.first_choice = new Hud(this.choices.element, 'first_choice')
+    // console.log(this.first_choice)
+    // this.first_choice.init(this.choices.element)
+    // this.first_choice.element.innerHTML = `<h1> Train </h1>`
+
+    // this.second_choice = new Hud(this.choices.element, 'second_choice')
+    // console.log(this.second_choice)
+    // this.second_choice.init(this.choices.element)
+
+    // this.third_choice = new Hud(this.choices.element, 'third_choice')
+    // console.log(this.third_choice)
+    // this.third_choice.init(this.choices.element)
+
+    // const image = new Image()
+    // image.onload = () => {
+    //   this.ctx.drawImage(image, 0, 0)
+    // }
+    // image.src = '/images/seamlessdarkbg.png'
+
+    // //Place some gameObjects
+
+    // const hero = new GameObject({
+    //   x: 650,
+    //   y: 350,
+    // })
+    // setTimeout(() =>{
+    //   hero.sprite.draw(this.ctx)
+    // }, 1000)
+    
   }
 }
